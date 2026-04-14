ad5761bdbc8fafeeb965efee959ad9b4c379e36b
dentrodailha96 <hnakashima.2022@gmail.com>
2026-04-14 15:05:32 +0200

    changes

---
+++ sql/schema.sql
@@ -0,0 +1,40 @@
+-- Create tables
+
+CREATE TABLE IF NOT EXISTS arimura_cj.products(
+    id_product SERIAL NOT NULL 
+  , name VARCHAR(500) NOT NULL
+  , price_product DECIMAL(10,2) NOT NULL
+  , sales_unit VARCHAR(5000)
+  , created_at   DATETIME DEFAULT CURRENT_TIMESTAMP
+  , last_modified TIMESTAMP NULL);
+
+CREATE TABLE IF NOT EXISTS arimura_cj.client(
+    id_client SERIAL
+  , name VARCHAR(500) NOT NULL
+  , telephone varchar(15) NOT NULL
+  , address varchar(500) NULL
+  , email varchar(500) NULL
+  , created_at   DATETIME DEFAULT CURRENT_TIMESTAMP
+  , last_modified TIMESTAMP NULL);
+
+CREATE TABLE IF NOT EXISTS arimura_cj.orders(
+    id_order SERIAL NOT NULL
+  , id_client INT NOT NULL
+  , status VARCHAR(100) NULL
+  , status       TEXT CHECK(status IN ('pending','done','cancelled')) DEFAULT 'pending'
+  , price        REAL DEFAULT 0
+  , delivery INTEGER DEFAULT 0 
+  , delivery_date DATE NOT NULL
+  , delivery_time TIME NOT NULL
+  , delivery_address VARCHAR(500)
+  , created_at TIMESTAMP NULL
+  , last_modified DATETIME DEFAULT CURRENT_TIMESTAMP);
+
+CREATE TABLE IF NOT EXISTS arimura_cj.order_product(
+    id SERIAL NOT NULL
+  , id_order INT 
+  , id_product INT 
+  , quantity INT NOT NULL
+  , unit_price DECIMAL(10,2) NULL
+  , observation VARCHAR (4000)
+  , created_at TIMESTAMP NULL);
